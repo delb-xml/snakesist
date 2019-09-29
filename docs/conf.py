43a5218d27034b56036cb84f337d4ad1ea8a2c6c
Theo Costea <theo.costea@gmail.com>
2019-09-29 11:43:50 +0200

    Fix docs
    Fix RTD documentation building

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+from pkg_resources import get_distribution
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -22,7 +23,8 @@ copyright = '2019, Theo Costea'
 author = 'Theo Costea'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = get_distribution('snakesist').version
+version = ''
 
 master_doc = 'index'
 
@@ -43,7 +45,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
