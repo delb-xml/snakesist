25c97bd8e9fb77e0499c44acd8cdbc9089e7b226
Theo Costea <theo.costea@gmail.com>
2019-09-29 09:15:33 +0200

    Update rtd config

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_default_options = {
+    'inherited-members': True,
+    'members': None,
+    'show-inheritance': True,
+    'undoc-members': True,
+}
