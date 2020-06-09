bc8ba09cab96e99399a46ad4c49899d91cd9c64a
Frank Sachsenheim <funkyfuture@riseup.net>
2020-06-09 22:17:42 +0200

    Improves test_delb_plugin.py
    - includes exist-db instance prefix in URLs
    - adapts delb.Document.source_ulr
    - paints it black

--- tests/test_delb_plugin.py
+++ tests/test_delb_plugin.py
@@ -1,23 +1,24 @@
 import pytest
 
-
-from delb import Document
+from delb import Document, FailedDocumentLoading
 from snakesist import ExistClient
 
 
 @pytest.mark.usefixtures("db")
 def test_delete_document():
-    url = "existdb://admin:@localhost:8080/test_collection/test_document.xml"
+    url = "existdb://admin:@localhost:8080/exist/test_collection/test_document.xml"
     document = Document(url)
     document.existdb_delete()
+    with pytest.raises(FailedDocumentLoading):
+        Document(url)
 
 
 @pytest.mark.usefixtures("db")
 def test_load_document_from_url():
-    url = "existdb://admin:@localhost:8080/test_collection/test_document.xml"
+    url = "existdb://admin:@localhost:8080/exist/test_collection/test_document.xml"
     document = Document(url)
 
-    assert document.config.source_url == url
+    assert document.source_url == url
     assert isinstance(document.config.existdb.abs_id, str)
     assert isinstance(document.config.existdb.client, ExistClient)
     assert document.existdb_collection == "/test_collection/"
@@ -32,9 +33,9 @@ def test_load_document_with_client():
     )
 
     assert (
-        document.config.source_url
-        == "existdb://admin:@localhost:8080/test_collection/test_document.xml"
-    )  # really??
+        document.source_url
+        == "existdb://admin:@localhost:8080/exist/test_collection/test_document.xml"
+    )
     assert isinstance(document.config.existdb.abs_id, str)
     assert document.config.existdb.client is exist_client
     assert document.existdb_collection == "/test_collection/"
@@ -46,17 +47,17 @@ def test_store_document():
     document = Document(
         "<test/>",
         existdb_client=ExistClient(
-            host="localhost", port=8080, user="admin", password="")
+            host="localhost", port=8080, user="admin", password=""
+        ),
     )
     document.existdb_store(collection="/test_collection/", filename="new_document.xml")
 
     document = Document(
-        "existdb://admin:@localhost:8080/test_collection/test_document.xml"
+        "existdb://admin:@localhost:8080/exist/test_collection/test_document.xml"
     )
     document.existdb_store(replace_existing=True)
     document.existdb_store(
-        collection="/another/collection/",
-        filename="another_name.xml",
+        collection="/another/collection/", filename="another_name.xml",
     )
     assert document.existdb_collection == "/test_collection/"
     assert document.existdb_filename == "test_document.xml"
